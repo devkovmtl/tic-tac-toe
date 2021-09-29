b8fada9e695de2f91e4267545e7df9c8b210c6d8
Mikael <dev.kov.mtl@gmail.com>
2021-09-29 15:11:15 -0400

    Tic Tac Toe - Display grid

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,23 @@
+const gameboardGridROW = document.querySelector('.row-gameboard-grid')
 const gameboardGrid = document.querySelector('.gameboard-grid')
+const btnStart = document.querySelector('.btn-start')
+const startGameRow = document.querySelector('.start-game')
+
+const player = (name, type = 'X') => {
+  const listMovePlayed = []
+
+  const addMove = (move) => {
+    listMovePlayed.push(move)
+  }
+
+  return { name, type, addMove, listMovePlayed }
+}
 
 const game = (() => {
+  let lastPlayed = ''
+
+  let winner = ''
+
   const gameboard = ['', '', '', '', '', '', '', '', '']
 
   const gameEnd = (gameboard) =>
@@ -29,27 +46,56 @@ const game = (() => {
       gameboard[5] === gameboard[7] &&
       gameboard[3])
 
-  const handleCellClick = (e) => {
-    console.log(e.target.dataset.cell)
-    e.target.textContent = 'X'
-    gameboard[e.target.dataset.cell] = 'X'
+  const startGame = () => {
+    while (!winner || !winner.length) {}
+    alert('WE have a winner', winner)
   }
 
-  const displayGameboard = () => {
+  const handleCellClick = (e) => {
+    // if (playerCanPlay) {
+    if (!lastPlayed || !lastPlayed.length || lastPlayed === 'O') {
+      // console.log(e.target.dataset.cell)
+      e.target.textContent = 'X'
+      gameboard[e.target.dataset.cell] = 'X'
+      lastPlayed = 'X'
+    } else {
+      e.target.textContent = 'O'
+      gameboard[e.target.dataset.cell] = 'O'
+      lastPlayed = 'O'
+    }
+    // }
+    // return
+  }
+
+  const displayGameboard = (gameboardGrid) => {
     let div
     for (let i = 1; i < gameboard.length + 1; i++) {
       div = document.createElement('div')
       div.setAttribute('class', 'cell')
       div.setAttribute('data-cell', i)
-      div.textContent = i
-      div.addEventListener('click', handleCellClick)
+      div.addEventListener('click', (e) => {
+        // console.log(!gameEnd(gameboard))
+        if (!gameEnd(gameboard)) {
+          handleCellClick(e)
+          winner = gameEnd(gameboard)
+          console.log(winner)
+        }
+      })
       gameboardGrid.appendChild(div)
     }
   }
 
   return {
     displayGameboard,
+    gameEnd,
+    gameboard,
   }
 })()
 
-game.displayGameboard(gameboardGrid)
+function onGameStart(e) {
+  game.displayGameboard(gameboardGrid)
+  startGameRow.style.display = 'none'
+  gameboardGridROW.style.display = 'flex'
+}
+
+btnStart.addEventListener('click', onGameStart)
--- scripts/player.js
+++
@@ -1,3 +0,0 @@
-const player = (type = 'x') => {
-  return { type }
-}
